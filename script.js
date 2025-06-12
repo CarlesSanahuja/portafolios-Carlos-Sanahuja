12940bde2a4018788a209ae4a2f9b3ece56721ea
CSanahuja <csanahuja@sapalomera.cat>
2025-06-12 13:08:24 +0200

    añadido el currículum

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-document.querySelectorAll('.profundidad').forEach(li => {
-  const texto = li.textContent.trim();
-  li.textContent = ''; // Vacía el li
+function historia(){
+    let nombre = document.querySelector(".corner-info");
+    nombre.style.position ="relative"
+    nombre.style.margin = "5%"
+    document.getElementById("curriculum").innerHTML='<img src="images/Carlos_Curriculum.jpg" alt="" width="1300" ><a href="./index.html"><img src="images/x.png" alt="" width="50"> </a>'
+    let mainContent = document.querySelector(".main-content");
+    mainContent.style.display = "none";
 
-  // Crea un span para cada letra y aplica un scale progresivo
-  for (let i = 0; i < texto.length; i++) {
-    const span = document.createElement('span');
-    span.textContent = texto[i] === ' ' ? '\u00A0' : texto[i]; // Espacio no rompible
-    // Calcula el factor de escala: empieza en 0.8 y termina en 1.35 (ajusta a gusto)
-    const scale = 0.8 + (0.55 * i) / (texto.length - 1 || 1);
-    span.style.transform = `scale(${scale}) skewY(-8deg)`;
-    li.appendChild(span);
-  }
-});
+}
