3e38f086fd6d2f725e45aaa983e6bdda30b88f74
CSanahuja <csanahuja@sapalomera.cat>
2025-06-13 12:04:51 +0200

    modificación tamaño currículum

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function historia(){
     let nombre = document.querySelector(".corner-info");
     nombre.style.position ="relative"
     nombre.style.margin = "5%"
-    document.getElementById("curriculum").innerHTML='<img src="images/Carlos_Curriculum.jpg" alt="" width="1300" ><a href="./index.html"><img src="images/x.png" alt="" width="50"> </a>'
+    document.getElementById("curriculum").innerHTML='<img src="images/Carlos_Curriculum.jpg" alt="" width="80%" ><a href="./index.html"><img src="images/x.png" alt="" width="50"> </a>'
     let mainContent = document.querySelector(".main-content");
     mainContent.style.display = "none";
 
